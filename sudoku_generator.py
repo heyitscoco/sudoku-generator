1c4a3b6124c78714f7d60c2fdbd44b7325e74843
Coco Conway <coco@Cocos-MBP.attlocal.net>
2023-08-13 14:12:23 -0700

    Incorporate argparse

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,6 +1,8 @@
 # !/usr/bin/python
-import sys
+import argparse
 from Sudoku.Generator import *
+
+DEFAULT_BASE_FILE = 'base.txt'
 
 # setting difficulties and their cutoffs for each solve method
 difficulties = {
@@ -10,32 +12,39 @@ difficulties = {
     'extreme': (81, 15)
 }
 
-# getting desired difficulty from command line
-difficulty = difficulties[sys.argv[2]]
+def main(difficulty, base=DEFAULT_BASE_FILE):
+    # constructing generator object from puzzle file (space delimited columns, line delimited rows)
+    difficulty = difficulties[difficulty]
+    gen = Generator(base)
 
-# constructing generator object from puzzle file (space delimited columns, line delimited rows)
-gen = Generator(sys.argv[1])
+    # applying 100 random transformations to puzzle
+    gen.randomize(100)
 
-# applying 100 random transformations to puzzle
-gen.randomize(100)
+    # getting a copy before slots are removed
+    initial = gen.board.copy()
 
-# getting a copy before slots are removed
-initial = gen.board.copy()
+    # applying logical reduction with corresponding difficulty cutoff
+    gen.reduce_via_logical(difficulty[0])
 
-# applying logical reduction with corresponding difficulty cutoff
-gen.reduce_via_logical(difficulty[0])
-
-# catching zero case
-if difficulty[1] != 0:
-    # applying random reduction with corresponding difficulty cutoff
-    gen.reduce_via_random(difficulty[1])
+    # catching zero case
+    if difficulty[1] != 0:
+        # applying random reduction with corresponding difficulty cutoff
+        gen.reduce_via_random(difficulty[1])
 
 
-# getting copy after reductions are completed
-final = gen.board.copy()
+    # getting copy after reductions are completed
+    final = gen.board.copy()
 
-# printing out complete board (solution)
-print("The initial board before removals was: \r\n\r\n{0}".format(initial))
+    # printing out complete board (solution)
+    print("The initial board before removals was: \r\n\r\n{0}".format(initial))
 
-# printing out board after reduction
-print("The generated board after removals was: \r\n\r\n{0}".format(final))
+    # printing out board after reduction
+    print("The generated board after removals was: \r\n\r\n{0}".format(final))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('difficulty', choices=difficulties.keys())
+    parser.add_argument('--base', default=DEFAULT_BASE_FILE, help='Path to a base puzzle file (space delimited columns, line delimited rows)')
+    args = parser.parse_args()
+    main(**args.__dict__)
